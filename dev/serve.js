1c50ef04355bba883031e28fe8e3df8cedbe838e
Ben Demboski <ben@turbopatent.com>
2019-06-26 08:30:26 -0700

    Allow running locally against any stage
    So the dev server can point to any google APIs/sheet backend

--- dev/serve.js
+++ dev/serve.js
@@ -3,5 +3,9 @@
 const buildApp = require('../src/build-app');
 const createSpreadsheet = require('./create-spreadsheet');
 
-let app = buildApp(createSpreadsheet);
+let [ , , stage = 'staging' ] = process.argv;
+
+console.log(`Running against stage: ${stage}`);
+
+let app = buildApp(() => createSpreadsheet(stage));
 app.listen(3000);
